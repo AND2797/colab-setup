6ee99ef3c8408792789226e34b21f4071a350199
AND2797 <aditya.das2797@gmail.com>
2021-06-07 00:28:17 -0400

    kaggle stuff

--- setup.sh
+++ setup.sh
@@ -11,6 +11,8 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 pip3 install kaggle
+mkdir -p .kaggle
+
 git clone https://github.com/AND2797/dotfiles.git
 sh dotfiles/install.sh
 
