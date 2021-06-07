5aa6b9a159ac1acc521bada2f67f1944ed1e5c08
AND2797 <aditya.das2797@gmail.com>
2021-06-06 23:45:40 -0400

    remove devnulls

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage > /dev/null 2>&1
+wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage 
 
 chmod u+x nvim.appimage
 
@@ -8,7 +8,7 @@ source .bashrc
 mkdir -p .config
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' > /dev/null 2>&1
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 pip3 install kaggle
 git clone https://github.com/AND2797/dotfiles.git
