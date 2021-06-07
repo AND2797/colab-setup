f92d66aaa1f5853d4c4034c36057b0d7570d2241
AND2797 <aditya.das2797@gmail.com>
2021-06-06 23:17:44 -0400

    fix typo

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ source .bashrc
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' > /dev/null 2>&1
 
-pip isntall kaggle
+pip install kaggle
 git clone https://github.com/AND2797/dotfiles.git
 cd dotfiles && sh install.sh
 
