567543b6f4a2bded97c0e3f7846f026a6ed59c81
AND2797 <aditya.das2797@gmail.com>
2021-06-06 23:36:45 -0400

    create .config

--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@ wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage > 
 chmod u+x nvim.appimage
 
 echo "alias nvim=/root/nvim.appimage" > ~/.bashrc
+
 source .bashrc
+mkdir -p .config
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' > /dev/null 2>&1
