7588f7e8a3eaead5e2c3713ff83245f0a5c1cbd0
AND2797 <aditya.das2797@gmail.com>
2021-06-06 23:15:55 -0400

    add kaggle api installation

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage > /dev/nukk 2>&1
 
 chmod u+x nvim.appimage
 
@@ -6,9 +6,9 @@ echo "alias nvim=/root/nvim.appimage" > ~/.bashrc
 source .bashrc
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' > /dev/null 2>&1
 
-
+pip isntall kaggle
 git clone https://github.com/AND2797/dotfiles.git
 cd dotfiles && sh install.sh
 
